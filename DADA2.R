f205787fb4b3382111a333141346f10b4d0e332e
M. Laura Rolon <79721177+LauRolon@users.noreply.github.com>
2024-03-25 11:27:41 -0700

    Update DADA2.R

--- DADA2.R
+++ DADA2.R
@@ -3,9 +3,6 @@
 
 #Last updated: MLR 12/2/21
 
-### Obj1 ####
-setwd('/storage/home/mlr355/work/NESARE/3.DADA2/Obj1')
-
 #Install DADA2
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
@@ -15,106 +12,7 @@ BiocManager::install("dada2")
 #Attach libraries
 library(dada2)
 
-#Make path for fastq files
-path <- '/storage/home/mlr355/work/NESARE/3.DADA2/Obj1' 
-list.files(path)
-
-# Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
-fnFs <- sort(list.files(path, pattern="_R1_001.fastq", full.names = TRUE))
-fnRs <- sort(list.files(path, pattern="_R2_001.fastq", full.names = TRUE))
-
-# Extract sample names
-sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
-
-#Inspect read quality for forward and reverse reads of first thee files
-plotQualityProfile(fnFs[1:3])
-plotQualityProfile(fnRs[1:3])
-
-#Filter and trim
-# Place filtered files in filtered/ subdirectory
-filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
-filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
-names(filtFs) <- sample.names
-names(filtRs) <- sample.names
-
-#Trim and filter for Forward at 200bp, for Reverse at 180 - Check based on graph
-out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(200,180),
-                     maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
-                     compress=TRUE, multithread=TRUE) # On Windows set multithread=FALSE
-head(out)
-
-#Learn error rates
-errF <- learnErrors(filtFs, multithread=TRUE)
-errR <- learnErrors(filtRs, multithread=TRUE)
-plotErrors(errF, nominalQ=TRUE)
-
-
-#Sample inference
-dadaFs <- dada(filtFs, err=errF, multithread=TRUE)
-dadaRs <- dada(filtRs, err=errR, multithread=TRUE)
-
-dadaFs[[1]]
-
-
-#Merge paired reads
-mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
-# Inspect the merger data.frame from the first sample
-head(mergers[[1]])
-
-#construct sequence table
-seqtab <- makeSequenceTable(mergers)
-dim(seqtab)
-
-# Inspect distribution of sequence lengths
-table(nchar(getSequences(seqtab)))
-
-#Remove non-target sequences from the sequence table
-seqtab2 <- seqtab[,nchar(colnames(seqtab)) %in% 250:256]
-
-#Remove chimeras
-seqtab.nochim <- removeBimeraDenovo(seqtab2, method="consensus", multithread=TRUE, verbose=TRUE)
-dim(seqtab.nochim)
-sum(seqtab.nochim)/sum(seqtab)
-
-#Track reads through the pipeline - check that there is no great drop in reads throughout any of the previous steps
-getN <- function(x) sum(getUniques(x))
-track <- cbind(out, sapply(dadaFs, getN), sapply(dadaRs, getN), sapply(mergers, getN), rowSums(seqtab.nochim))
-# If processing a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
-colnames(track) <- c("input", "filtered", "denoisedF", "denoisedR", "merged", "nonchim")
-rownames(track) <- sample.names
-head(track)
-
-#Assign taxonomy
-taxa <- assignTaxonomy(seqtab.nochim, "/storage/home/mlr355/work/NESARE/3.DADA2/tax/silva_nr_v132_train_set.fa.gz", multithread=TRUE)
-
-#Add species column
-taxa <- addSpecies(taxa, "/storage/home/mlr355/work/NESARE/3.DADA2/tax/silva_species_assignment_v132.fa.gz")
-taxa.print <- taxa # Removing sequence rownames for display only
-rownames(taxa.print) <- NULL
-taxa.print
-
-#Make phyloseq
-library(phyloseq)
-ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
-               tax_table(taxa))
-
-#Change name from sequence to ASV#
-dna <- Biostrings::DNAStringSet(taxa_names(ps))
-names(dna) <- taxa_names(ps)
-ps <- merge_phyloseq(ps, dna)
-taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
-ps
-
-
-#Save ASV table as csv
-asvs<-as.data.frame(otu_table(ps))
-Taxon<-as.data.frame(tax_table(ps))
-
-write.csv(asvs, file = "ASV.csv")
-write.csv(Taxon, file = "Taxon.csv")
-
-
-### All sequences from project ####
+#Set working directory
 setwd('/storage/home/mlr355/work/NESARE/3.DADA2/All/')
 
 #Attach libraries
